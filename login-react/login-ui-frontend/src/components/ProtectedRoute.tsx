147e8e720fffb4ce219d623e09e1bd203a34d8b3
gps-vr2 <gps.vr2.in@gmail.com>
2025-07-31 16:06:41 +0530

    Update ProtectedRoute.tsx

--- login-react/login-ui-frontend/src/components/ProtectedRoute.tsx
+++ login-react/login-ui-frontend/src/components/ProtectedRoute.tsx
@@ -5,7 +5,7 @@ import { useAuth } from '../contexts/AuthContext';
 const ProtectedRoute = () => {
   const { user, token } = useAuth();
 
-  // If there's no token or user, redirect to the login page
+  // If  there's no token or user, redirect to the login page
   if (!token || !user) {
     return <Navigate to="/" replace />;
   }
