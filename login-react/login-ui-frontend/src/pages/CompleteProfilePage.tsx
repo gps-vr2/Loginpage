e1692a54e6a6c028fca30ba90463fcdd0a6a413e
ajithkumar200513 <ajith200536lap@gmail.com>
2025-08-12 12:39:25 +0530

    bdeuj

--- login-react/login-ui-frontend/src/pages/CompleteProfilePage.tsx
+++ login-react/login-ui-frontend/src/pages/CompleteProfilePage.tsx
@@ -1,5 +1,5 @@
 import type { CSSProperties } from "react";
-
+import React, { useState} from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import axios from "axios";
